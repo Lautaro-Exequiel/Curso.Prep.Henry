04ddbdd9824b56b9ea0012fd339db2fb3c815332
Lautaro-Exequiel <Lautaro.Exequiel.C@hotmail.com>
2021-05-17 23:58:04 -0300

    Homework JS-VI Hecha

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,20 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  return nombre.charAt(0).toUpperCase() + nombre.slice(1)
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  return cb()
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1, n2)
 }
 
 function sumarArray(numeros, cb) {
@@ -22,25 +25,44 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  var suma_reduce = numeros.reduce(function (acc, cv) {
+    return acc + cv
+  }, 0)
+  cb(suma_reduce)
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(function (element, index) {
+    cb(element)
+  })
 }
+  
 
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var nuevo_array = array.map(function (element) {
+    return cb(element)
+  })
+  return nuevo_array
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  nuevo_array = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i][0] === "a") {
+      nuevo_array.push(array[i])
+    }
+  } 
+  return nuevo_array
 }
 
 // No modificar nada debajo de esta línea
