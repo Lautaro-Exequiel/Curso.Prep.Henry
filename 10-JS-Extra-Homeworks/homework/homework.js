593344c649ea1564d3e6a6490964e24f8a693fd6
Lautaro-Exequiel <Lautaro.Exequiel.C@hotmail.com>
2021-06-09 19:17:41 -0300

    JSX

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,7 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  return Object.entries(objeto)
 }
 
 
@@ -18,6 +19,11 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var obj = {};
+  for (var i in string) {
+    obj[string[i]] = (obj[string[i]] || 0) + 1;
+  }
+  return obj
 }
 
 
@@ -26,6 +32,18 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var ch = s.split("");
+  var cap_ch = [];
+  var lower_ch = [];
+  for (var i = 0; i < ch.length; i++) {
+    if (ch[i] === ch[i].toUpperCase()) {
+      cap_ch.push(ch[i])
+    }
+    if (ch[i] === ch[i].toLowerCase()) {
+      lower_ch.push(ch[i])
+    }
+  }
+  return cap_ch.join("").concat(lower_ch.join(""))
 }
 
 
@@ -35,7 +53,9 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
-} 
+  return str.split("").reverse().join("").split(" ").reverse().join(" ")
+}
+
 
 
 function capicua(numero){
@@ -43,13 +63,25 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  if (numero.toString().split("").reverse().join("") === numero.toString()) {
+   return "Es capicua"
+  }
+  else return "No es capicua"
 }
+
 
 
 function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  cadena2 = "";
+  for (var i = 0; i < cadena.length; i++) {
+    if (cadena[i] !== "a" && cadena[i] !== "b" && cadena[i] !== "c") {
+      cadena2 += cadena[i]
+    }
+  }
+  return cadena2
 }
 
 
